8ad5dff5f82f4248207792ea1ee234dded550a6b
Rob Jacobson <rljacobson@outlook.com>
2025-10-18 13:22:14 -0700

    test: add validation and missing parameters tests to standard endpoint tests

--- tests/e2e/shared/test-templates.ts
+++ tests/e2e/shared/test-templates.ts
@@ -1,11 +1,13 @@
 import type { Endpoint } from "../../../src/shared/types";
 import { createDataIntegrityTest } from "./helpers/data-integrity-test";
 import { createDefaultParametersTest } from "./helpers/default-parameters-test";
+import { createDefaultParametersValidationTest } from "./helpers/default-parameters-validation-test";
 import { createErrorHandlingTest } from "./helpers/error-handling-test";
 import {
   createRealDataWithoutValidationTest,
   createRealDataWithValidationTest,
 } from "./helpers/fetch-real-data-test";
+import { createMissingParametersTest } from "./helpers/missing-parameters-test";
 /**
  * Creates all standard tests for an endpoint
  */
@@ -13,8 +15,10 @@ export const createStandardEndpointTests = (
   endpoint: Endpoint<unknown, unknown>
 ) => {
   createDefaultParametersTest(endpoint);
+  createDefaultParametersValidationTest(endpoint);
   createRealDataWithoutValidationTest(endpoint);
   createRealDataWithValidationTest(endpoint);
   createDataIntegrityTest(endpoint);
   createErrorHandlingTest(endpoint);
+  createMissingParametersTest(endpoint);
 };
